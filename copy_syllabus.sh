800110911457e8ad28dbbcf28ef431809e23045f
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-12-15 15:46:23 -0800

    Update both PDF and HTML syllabus

--- copy_syllabus.sh
+++ copy_syllabus.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 # Run R markdown file
-Rscript -e "rmarkdown::render('_logistics/A_Syllabus.Rmd')"
+sudo Rscript -e "rmarkdown::render('_logistics/A_Syllabus.Rmd', output_format = 'html_document')"
 
-echo "Compiled the R markdown file"
+echo "Compiled the R markdown file in HTML format"
+
+sudo Rscript -e "rmarkdown::render('_logistics/A_Syllabus.Rmd', output_format = 'pdf_document')"
+
+echo "Compiled the R markdown file in PDF format"
 
 # Move the outptus to the project directory
 cp _logistics/A_Syllabus.pdf .
+
+echo "Copied the PDF version of the syllabus"
+
 cp _logistics/A_Syllabus.html .
 
-echo "Copied syllabus"
+echo "Copied the HTML version of the syllabus"
