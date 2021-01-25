301eba1252de54505dd578e7659f3a61d45deaf1
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-01-25 12:28:53 -0800

    add check_installation.sh

---
+++ check_installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+R --version | head -n 1
+
+git --version | head -n 1
+
+python --version | head -n 1
+
+nano --version | head -n 1
+
+pdflatex --version | head -n 1
+
+pandoc --version | head -n 1
